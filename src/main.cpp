d66e0e3ea647f787b9eb4b700be697b1d1b52d17
sacrartorium <72121958+sacrartorium@users.noreply.github.com>
2020-12-23 20:54:37 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "db/database.hpp"
+#include "db/table_manager.hpp"
+#include "net/server.hpp"
+#include "net/session.hpp"
+
+int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    std::cout << "Error: join_server <port>" << std::endl;
+    return -1;
+  }
+
+  DataBasePtr db = std::make_shared<DataBase>(std::make_shared<TableManager>());
+  db->request("create A");
+  db->request("create B");
+
+  boost::asio::io_service service;
+
+  size_t port = atoi(argv[1]);
+  boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), port);
+  Server server(service, ep, db);
+
+  service.run();
+
+  return 0;
+}
---
+++ src/utilities.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <sstream>
+
+template <typename T> T getNextToken(std::stringstream &stream) {
+  T token;
+  stream >> token;
+  return token;
+}
