8347081ee8eed1f3f19d481582fe833320517a60
sacrartorium <72121958+sacrartorium@users.noreply.github.com>
2020-12-23 20:56:17 +0300

    Add files via upload

---
+++ src/server.cpp
@@ -0,0 +1,21 @@
+﻿#include "server.hpp"
+
+#include "session.hpp"
+
+Server::Server(boost::asio::io_service &service,
+               const boost::asio::ip::tcp::endpoint &ep, DataBasePtr db)
+    : mAcceptor(service, ep), mSocket(service), mSessions(), mDataBase(db) {
+  accept();
+}
+
+void Server::accept() {
+  mAcceptor.async_accept(mSocket, [this](boost::system::error_code errorCode) {
+    if (!errorCode) {
+      auto session =
+          std::make_shared<Session>(std::move(mSocket), mSessions, mDataBase);
+      mSessions.insert(session);
+      session->begin();
+    }
+    accept();
+  });
+}
