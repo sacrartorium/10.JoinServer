af59a90feef6c8557fe516e5c1421ad50f1b9531
sacrartorium <72121958+sacrartorium@users.noreply.github.com>
2020-12-23 20:57:17 +0300

    Add files via upload

---
+++ src/basic_request.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "../db/status.hpp"
+#include "../db/table_manager.hpp"
+#include "../utilities.hpp"
+
+struct BasicRequest {
+  BasicRequest(std::string name) : mRequestName(name) {}
+
+  virtual Status start(TableManagerPtr tableManager,
+                       std::stringstream &stream) = 0;
+  virtual std::string hint() = 0;
+
+  std::string getRequestName() { return mRequestName; }
+
+private:
+  std::string mRequestName;
+};
+using BasicRequestPtr = std::shared_ptr<BasicRequest>;
