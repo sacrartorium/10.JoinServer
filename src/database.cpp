0d7645502386ad22a1c059c7b0f4d2ba05135743
sacrartorium <72121958+sacrartorium@users.noreply.github.com>
2020-12-23 20:55:01 +0300

    Add files via upload

---
+++ src/database.cpp
@@ -0,0 +1,32 @@
+﻿#include "database.hpp"
+
+DataBase::DataBase(TableManagerPtr tableManager) : mTableManager(tableManager) {
+  create<RequestCreate>();
+  create<RequestInsert>();
+  create<RequestIntersection>();
+  create<RequestSymmetricDifference>();
+  create<RequestTruncate>();
+}
+
+DataBase::~DataBase() { mRequests.clear(); }
+
+template <typename T> void DataBase::create() {
+  BasicRequestPtr request = std::make_shared<T>();
+  mRequests[request->getRequestName()] = request;
+}
+
+std::string DataBase::request(std::string request) {
+  LockGuard lock(m);
+
+  if (request.back() != '\n')
+    request.push_back('\n');
+
+  std::stringstream streamRequest(request);
+
+  std::string command = getNextToken<std::string>(streamRequest);
+
+  if (mRequests.count(command) == 0)
+    return Status::error("Command '" + command + "' not found!");
+  else
+    return mRequests[command]->start(mTableManager, streamRequest).stringify();
+}
